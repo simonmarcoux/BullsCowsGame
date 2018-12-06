b579cbf676a98cd6d3bb2f795e9621185084bb95
Simon Marcoux <sim_marc@hotmail.com>
2018-12-06 15:21:47 -0500

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Simon Marcoux on 18-12-06.
+//  Copyright © 2018 Simon Marcoux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
