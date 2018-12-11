4b169b0fd27f1031e7676f84e1ccfd42ee897d0d
Simon Marcoux <sim_marc@hotmail.com>
2018-12-11 14:09:34 -0500

    Init Bulls and Cows game repo

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,9 @@
+//
+//  FBullCowGame.cpp
+//  BullCowGame
+//
+//  Created by Simon Marcoux on 18-12-10.
+//  Copyright © 2018 Simon Marcoux. All rights reserved.
+//
+
+#include "FBullCowGame.hpp"
---
+++ BullCowGame/FBullCowGame.hpp
@@ -0,0 +1,14 @@
+//
+//  FBullCowGame.hpp
+//  BullCowGame
+//
+//  Created by Simon Marcoux on 18-12-10.
+//  Copyright © 2018 Simon Marcoux. All rights reserved.
+//
+
+#ifndef FBullCowGame_hpp
+#define FBullCowGame_hpp
+
+#include <stdio.h>
+
+#endif /* FBullCowGame_hpp */
